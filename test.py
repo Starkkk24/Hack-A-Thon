4ee41aa7093fc41d158dcdaed24d7b283b373c50
tech029 <techwork029@gmail.com>
2025-01-25 07:01:08 -0800

    web5

--- test.py
+++ test.py
@@ -2,9 +2,13 @@ import requests
 
 url = "http://192.168.214.161:5000/auth/web/reg"
 data = {
-    "key1": "value1",
-    "key2": "value2"
+    "user": "sazzzzzzz354",
+    "name": "saswath",
+    "dept": "Normal",
+    "code": "hello",
+    "under": "zzzz"
 }
-
+params = []
+response = requests.get(url, params=params)
 response = requests.post(url, data=data)
 print(response.text)
