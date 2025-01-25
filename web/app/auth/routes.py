32936a754fafb4c8f01a2b0e0d203cc295a36f52
Stark <ankit566kota@gmail.com>
2025-01-25 23:45:49 +0530

    webv1

--- web/app/auth/routes.py
+++ web/app/auth/routes.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template
 
-app1_bp = Blueprint('app1', __name__, template_folder='templates')
+app1_bp = Blueprint('app1', __name__, template_folder='templates', static_folder="static")
 
 @app1_bp.route('/')
 def index():
     return "dcfvgbhj"
+
+
+@app1_bp.route('/reg')
+def reg():
+    return render_template("reg.html")
+
+@app1_bp.route('/log')
+def log():
+    return render_template("log.html")
